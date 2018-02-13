2ea919f3f3a3be62f73cc801c691a89b251cd385
Keenan Goldis <keenangoldis@gmail.com>
2018-02-12 20:36:34 -0600

    Add Flexbox CSS

--- src/components/ListOfSausageRecipes.js
+++ src/components/ListOfSausageRecipes.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import SausageRecipeDetail from "./SausageRecipeDetail.js";
 import "../css/ListOfSausageRecipes.css";
+import Flexbox from 'flexbox-react';
 
 class ListOfSausageRecipes extends Component {
   constructor(props) {
@@ -35,25 +36,26 @@ class ListOfSausageRecipes extends Component {
     return (
       <div>
       <button id="button" onClick={this.toggleList}>Sausage Recipes</button>
-        <div hidden={!this.state.listVisible}>
-          {this.state.sausageRecipes.map((sausageRecipe, i) =>
-            <div key={i}>
-              <p key={i}>
-                <div className="sausage_name">
-                  <h3>{sausageRecipe.sausage_name}</h3>
-                </div>
-                <div className="sausage_description">
-                  <h6>{sausageRecipe.sausage_description}</h6>
-                </div>
-
-                <button id="view" onClick={() => {this.props.selectSausageRecipe(sausageRecipe.id-1)}}>View Detail</button>
-              </p>
-            </div>
-          )}
-        </div>
-        <div className="detail" hidden={!this.state.detailVisible}>
-          <SausageRecipeDetail current={current} />
-        </div>
+        <Flexbox className="sausageRecipeList" >
+          <div hidden={!this.state.listVisible}>
+            {this.state.sausageRecipes.map((sausageRecipe, i) =>
+              <div key={i}>
+                <p key={i}>
+                  <div className="sausage_name">
+                    <h3>{sausageRecipe.sausage_name}</h3>
+                  </div>
+                  <div className="sausage_description">
+                    <h6>{sausageRecipe.sausage_description}</h6>
+                  </div>
+                  <button id="view" onClick={() => {this.props.selectSausageRecipe(sausageRecipe.id-1)}}>View Detail</button>
+                </p>
+              </div>
+            )}
+          </div>
+          <div className="detail" hidden={!this.state.detailVisible}>
+            <SausageRecipeDetail current={current} />
+          </div>
+        </Flexbox>
       </div>
     );
   }
--- src/components/SausageRecipeDetail.js
+++ src/components/SausageRecipeDetail.js
@@ -4,7 +4,6 @@ function SausageRecipeDetail(props){
   let sausageRecipe = props.current;
   return(
     <div>
-      <h2>Testing</h2>
       <img src={sausageRecipe.avatar} alt={sausageRecipe.sausage_name} />
       <p>{sausageRecipe.sausage_description}</p>
     </div>
