f176ab8347408c43a1d293d732570c004cf286c8
Keenan Goldis <keenangoldis@gmail.com>
2018-02-16 15:34:05 -0600

    Hooked up MySQL Database

--- src/script.js
+++ src/script.js
@@ -2,32 +2,33 @@ var express = require('express');
 var mysql = require('mysql');
 var app = express();
 
-var connection = mysql.createConnection({
+var connection = mysql.createPool({
   //properties...
+  connectionLimit: 50,
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'sausageRecipes'
+  database: 'sausagerecipes'
 });
 
-connect.connect(function(error){
-  //callback function
-  if(!!error){
+//tempCont is used so that the server won't crash if too many people use it at the same time.
+//https://www.youtube.com/watch?v-hGZX_SA7IYg at 18 minutes for explanation.
+app.get('/', function (request, response){
+connection.getConnection(function(error, tempCont){
+  if(!!error) {
+    tempCont.release();
     console.log('Error');
   } else {
-    console.log('Connected');
-  }
+    console.log('Connected!');
+    tempCont.query("SELECT * FROM allsausagerecipes", function(error, rows, fields){
+      tempCont.release();
+      if(!!error) {
+        console.log('Error in the query');
+      } else {
+        response.json(rows);
+      }
+    });
+    }
+  });
 });
-
-app.get('/', function (req, resp){
-  //  mysql example below
-  connection.query("SELECT * FROM sausageRecipes", function(error,rows, fields)
-  //callback function
-  if(!!error) {
-    console.log('Error in the query');
-  } else {
-    console.log('Successful query');
-  }
-})
-
 app.listen(1337);
